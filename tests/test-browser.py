e085068631d4c891dc66e588d88f59c2f393ac92
Marco Pesenti Gritti <marco@localhost.localdomain>
2007-05-20 11:48:41 +0200

    Demonstrate application components registration.

--- tests/test-browser.py
+++ tests/test-browser.py
@@ -2,6 +2,17 @@ import gtk
 import hulahop
 
 from xpcom import components
+from xpcom.server.factory import Factory
+
+class AppLauncher:
+    _com_interfaces_ = components.interfaces.nsIHelperAppLauncherDialog
+
+    def promptForSaveToFile(self, launcher, windowContext,
+                            defaultFile, suggestedFileExtension):
+        print 'promptForSaveToFile'
+                            
+    def show(self, launcher, context, reason):
+        print 'show'
 
 class EventListener:
     _com_interfaces_ = components.interfaces.nsIDOMEventListener
@@ -12,6 +23,18 @@ class EventListener:
 def _quit(window):
     hulahop.shutdown()
     gtk.main_quit()
+
+registrar = components.registrar
+registrar.registerFactory('{64355793-988d-40a5-ba8e-fcde78cac631}"',
+                          'Test Application Launcher',
+                          '@mozilla.org/helperapplauncherdialog;1',
+                          Factory(AppLauncher))
+
+cls = components.classes["@mozilla.org/preferences-service;1"]
+prefService = cls.getService(components.interfaces.nsIPrefService)
+branch = prefService.getBranch('')
+branch.setBoolPref('security.warn_submit_insecure', False)
+branch.setBoolPref('security.warn_submit_insecure.show_once', False)
 
 window = gtk.Window()
 window.connect("destroy", _quit)
